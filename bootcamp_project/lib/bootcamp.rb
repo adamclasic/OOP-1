2fc1a0512df3e8db470fa7e2c8e8cca55d0f6f95
adam <aabou9@gmail.com>
2020-04-16 17:44:42 +0100

    add init method

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,4 +1,12 @@
 class Bootcamp
 
-  
+    def initialize(name, slogan, teachers, students, grades)
+        @name = name
+        @slogan = slogan
+        @students = students
+        @students = students
+        @grades = grades
+    end
+
+    
 end
