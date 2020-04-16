f9abd00113f3bceebef30f64910570dd52b517d7
adam <aabou9@gmail.com>
2020-04-16 16:30:10 +0100

    update arbitry methods

--- dog_project/lib/dog.rb
+++ dog_project/lib/dog.rb
@@ -14,20 +14,36 @@ class Dog
     def name; @name end
     def breed; @breed end
     def age; @age end
-    def bark; "#{@bark} my name is #{@name} !" end
-    def favorite_foods; @favorite_foods end
+    def bark
+        @age>3 ? @bark.upcase : @bark.downcase
+    end
+    def favorite_foods; @favorite_foods.map { |i| i.capitalize} end
 
+    #setter methods
+
+    def age=(age)
+        @age = age
+    end
+    
 
     #arbitry methods
-    def favorite_foods?(str)
-        @favorite_foods.include?(str)
+    def favorite_food?(str)
+        @favorite_foods.include?(str.capitalize)
     end
 
 end
 
-dog1 = Dog.new('spenc', 'german sheprd', 4, "Miaw-Miaw", ['salad', 'chese', 'water'])
-p dog1
 
-p dog1.bark
+# dog1 = Dog.new('spenc', 'german sheprd', 4, "Miaw Miaw", ['salad', 'chese', 'water'])
 
-p dog1.favorite_foods?('milk')
+# p dog1.age
+
+# p dog1
+
+# p dog1.bark
+# p dog1.favorite_foods
+
+# p dog1.favorite_food?('Water')
+# dog1.age = 2
+
+# p dog1.bark
