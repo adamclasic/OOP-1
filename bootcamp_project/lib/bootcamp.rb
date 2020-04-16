636edb8491bb26f74d7de20a40cc28977ef8baae
adam <aabou9@gmail.com>
2020-04-16 18:11:10 +0100

    add arbitry methods

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,6 +1,6 @@
 class Bootcamp
 
-    def initialize(name, slogan, teachers, students, grades)
+    def initialize(name, slogan, teachers = [], students = [], grades = Hash.new {|value, key| value[key] = []})
         @name = name
         @slogan = slogan
         @teachers = teachers
@@ -16,7 +16,26 @@ class Bootcamp
     def students; @students end
     def grades; @grades end
 
-    
+
+    #arbitry methods
+
+    def hire(str)
+        @teachers << str
+    end
+
+    def enroll(str)
+        @students << str
+    end
+
+    def enrolled?(str)
+        @students.include(str)
+    end
+
+    def teacher_to_studnet_ratio()
+        @teachers.length/@students.length
+    end
 
     
 end
+
+boot1 = Bootcamp.new('microverse', 'free until u get a job')
