21aa176004218be3558c5afc42a964d39f0e0e78
adam <aabou9@gmail.com>
2020-04-17 15:20:30 +0100

    tested bootcamp.rb

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,6 +1,6 @@
 class Bootcamp
 
-    def initialize(name, slogan, student_capacity, teachers = [], students = [], grades = Hash.new {|value, key| value[key] = []})
+    def initialize(name, slogan, student_capacity, teachers = [], students = [], grades = {})
         @name = name
         @slogan = slogan
         @student_capacity = student_capacity
@@ -32,7 +32,7 @@ class Bootcamp
     end
 
     def enroll(str)
-        if @students.size+1 > @student_capacity
+        if @students.length+1 > @student_capacity
             return false
         else
             @students << str
@@ -52,20 +52,39 @@ class Bootcamp
 
     def add_grade(s_name, s_grades)
         if enrolled?(s_name)
-            @grades[s_name] << s_grades
+            @grades[s_name] = s_grades
             true
         else
             return false
         end
     end
 
+    def num_grades(s_name)
+        @grades[s_name].length
+    end
+
+    def average_grade(ss_name)
+        p a = @grades[ss_name]
+        if a.length == 0
+            return nil
+        else
+
+            r = (@grades[ss_name].sum(0.0) / @grades[ss_name].length)
+            return r
+        end
+    end
     
 end
 
-p boot1 = Bootcamp.new('microverse', 10, 'free until u get a job', %w(her brown krark browns), %w(her brown bfrown krar k browns))
-p boot1.student_to_teacher_ratio
-p boot1.students
-p boot1.teachers
-p boot1.add_grade('adam', [11, 01, 54])
-p boot1.add_grade('djazo', [11, 01, 54])
-p boot1.grades
+# p boot1 = Bootcamp.new('microverse', 'free until u get a job', 30, %w(her brown krark browns), %w(her brown bfrown krar k browns))
+# p boot1.student_to_teacher_ratio
+# p boot1.enroll 'adam'
+# p boot1.enroll 'djazo'
+# p boot1.students
+# p boot1.teachers
+# p boot1.add_grade('adam', [11, 31, 13])
+# p boot1.add_grade('djazo', [11, 12, 21])
+# p boot1.average_grade('djazo')
+# p boot1.average_grade('adam')
+# p boot1.grades
+# p [11, 01, 54].sum(0.0)
