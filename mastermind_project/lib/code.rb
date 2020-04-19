654269e72cadf077da5fef15cfafb943600037ee
adam <aabou9@gmail.com>
2020-04-19 16:00:36 +0100

    add init method

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -6,4 +6,22 @@ class Code
     "Y" => :yellow
   }
 
+  def self.valid_pegs?(arr)
+    arr.each { |val| return false unless POSSIBLE_PEGS.include?(val.upcase)}
+    true
+  end
+
+  def initialize(arr)
+    a = Code.valid_pegs?(arr)
+    Code.valid_pegs?(arr) ? @pegs = arr.map(&:upcase) : raise("You have enterned invalid pegs")
+  end
+  attr_reader :pegs
+
+
+
 end
+
+
+# cla = Code.new(['b', 'r', 't'])
+# p Code.valid_pegs?(['B', 'R', 'R'])
+# p cla.pegs
