4b8436fc8795bcf291a99ebde2384e1e5abcd44f
adam <aabou9@gmail.com>
2020-04-19 17:57:57 +0100

    add random

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -7,21 +7,37 @@ class Code
   }
 
   def self.valid_pegs?(arr)
-    arr.each { |val| return false unless POSSIBLE_PEGS.include?(val.upcase)}
+    arr.each { |val| return false unless POSSIBLE_PEGS.include?(val.upcase) }
     true
   end
 
   def initialize(arr)
-    a = Code.valid_pegs?(arr)
-    Code.valid_pegs?(arr) ? @pegs = arr.map(&:upcase) : raise("You have enterned invalid pegs")
+    if Code.valid_pegs?(arr)
+      @pegs = arr.map(&:upcase)
+    else
+      raise("You have enterned invalid pegs")
+    end
   end
+
+  # def hid; @@pegs = @pegs end
+
   attr_reader :pegs
 
-
-
+  def self.random(num)
+    arr =[]
+    num.times { arr <<  POSSIBLE_PEGS.keys[rand(POSSIBLE_PEGS.length)] }
+    Code.new(arr)  
+  end
 end
 
 
-# cla = Code.new(['b', 'r', 't'])
+# cla = Code.new(['b', 'b', 'y'])
 # p Code.valid_pegs?(['B', 'R', 'R'])
 # p cla.pegs
+p Code.random(32)
+
+
+# %w(jas jj ls fj).
+# a = rand(10)
+# p a
+# p a.class
